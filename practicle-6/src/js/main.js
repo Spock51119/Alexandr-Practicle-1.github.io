e850631540c8726dd70b19e07091a995a0f9d5c0
Spock51119 <57391617+Spock51119@users.noreply.github.com>
2020-11-05 12:11:18 +0500

    update
    1

--- practicle-6/src/js/main.js
+++ practicle-6/src/js/main.js
@@ -1,10 +1,10 @@
 import './vendor/focus-visible.min.js';
 import './_vars';
-import "./_pool-expert"
-import "./_works"
-import "./_plan"
-import "./_testimonials"
-import "./_burger"
+import "./_pool-expert";
+import "./_works";
+import "./_plan";
+import "./_testimonials";
+import "./_burger";
 // import {resizeContent} from './functions/resize';
 // import {scrollTo} from './functions/smooth-scroll';
 // import {disableScroll, enableScroll} from './functions/stop-scroll';
@@ -13,42 +13,4 @@ import "./_burger"
 
 
 
-// import Swiper JS
-// core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-Swiper.use([Navigation, Pagination]);
-
-const mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    type:"bullets",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 5000,
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-
-  breakpoints: {
-    800: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-    1120: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
-      spaceBetween: 30,
-    },
-  },
-})
 
